aa084bca1b139333e6128b6bcf386ea68e18f531
SambaRoyal <sambapathi1996@gmail.com>
2020-01-02 18:00:37 +0530

    added

--- src/test/java/Runner/RunnerClass.java
+++ src/test/java/Runner/RunnerClass.java
@@ -7,7 +7,8 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "C:\\Users\\dev01\\eclipse-workspace\\Poc\\src\\main\\java\\FeatureFile\\feat.feature", glue = {
-		"Stepdefination" },tags= {"@Test3"})
+		"Stepdefination" })
+//tags= {"@Test3"}
 public class RunnerClass {
 
 }
