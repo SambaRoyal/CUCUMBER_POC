484fcd5e7f3dd6a1b18a8ed689b3f316d8308e9d
SambaRoyal <sambapathi1996@gmail.com>
2020-01-10 10:05:12 +0530

    Updated the login page

--- src/main/java/Stepdefination/LoginDefination.java
+++ src/main/java/Stepdefination/LoginDefination.java
@@ -13,58 +13,29 @@ import cucumber.api.java.en.When;
 import pagelocators.StringUtil;
 import testUtil.Utils;
 
-public class LoginDefination extends BaseClass {
-	
-	private Utils util;
 
+public class LoginDefination extends BaseClass {
+
+	
 	public LoginDefination() throws IOException {
 		super();
 	}
+	public Utils util;
 
-	/*@Given("^user already in login page$")
-	public void user_already_in_login_page() throws Throwable {
-		browser(prop.getProperty("url"));
-	}*/
-
-	/*@When("^user able to verify title of application$")*/
-	/*public void user_able_to_verify_title_of_application() throws InterruptedException {
-		util = new Utils();
-		util.clickElement(By.xpath("//button[@id='main-popupclose']"), driver);
-		util.clickElement(By.xpath("//a[@id='login-btn']"), driver);
-	}*/
-
-	/*@Then("^user enters username$")
-	public void user_enters_username() {
-		util.inputText(By.xpath("//*[@id='j_username']"), prop.getProperty("username"), driver);
-	}*/
-
-	/*@Then("^user enters password$")
-	public void user_enters_password() {
-		util.inputText(By.xpath("//*[@id='j_password']"), prop.getProperty("password"), driver);
-		util.clickElement(By.xpath("//*[@id='login']"), driver);
-		String title = driver.getTitle();
-		if (title.contains("LetMeCall")) {
-			assertEquals(StringUtil.login_title, title);
-		} else {
-			System.out.println("in-correct title    :" + title);
-		}
-	}*/
-
-	
 	@Given("^user already in login page$")
-	public void user_already_in_login_page()  {
+	public void user_already_in_login_page() throws Throwable {
 		browser(prop.getProperty("url"));
 	}
 
 	@When("^user able to verify title of application$")
-	public void user_able_to_verify_title_of_application() {
+	public void user_able_to_verify_title_of_application() throws InterruptedException {
 		util = new Utils();
 		util.clickElement(By.xpath("//button[@id='main-popupclose']"), driver);
 		util.clickElement(By.xpath("//a[@id='login-btn']"), driver);
 	}
 
 	@Then("^user enters username$")
-	public void user_enters_username()  {
+	public void user_enters_username() {
 		util.inputText(By.xpath("//*[@id='j_username']"), prop.getProperty("username"), driver);
 	}
 
@@ -79,8 +50,28 @@ public class LoginDefination extends BaseClass {
 			System.out.println("in-correct title    :" + title);
 		}
 	}
-	/*@After
-	public void teardown() {
-		driver.close();
-	}*/
+
+	/*
+	 * @Given("^user already in login page$") public void
+	 * user_already_in_login_page() { browser(prop.getProperty("url")); }
+	 * 
+	 * @When("^user able to verify title of application$") public void
+	 * user_able_to_verify_title_of_application() { util = new Utils();
+	 * util.clickElement(By.xpath("//button[@id='main-popupclose']"), driver);
+	 * util.clickElement(By.xpath("//a[@id='login-btn']"), driver); }
+	 * 
+	 * @Then("^user enters username$") public void user_enters_username() {
+	 * util.inputText(By.xpath("//*[@id='j_username']"),
+	 * prop.getProperty("username"), driver); }
+	 * 
+	 * @Then("^user enters password$") public void user_enters_password() {
+	 * util.inputText(By.xpath("//*[@id='j_password']"),
+	 * prop.getProperty("password"), driver);
+	 * util.clickElement(By.xpath("//*[@id='login']"), driver); String title =
+	 * driver.getTitle(); if (title.contains("LetMeCall")) {
+	 * assertEquals(StringUtil.login_title, title); } else {
+	 * System.out.println("in-correct title    :" + title); } }
+	 * 
+	 * @After public void teardown() { driver.close(); }
+	 */
 }
--- src/test/java/Runner/RunnerClass.java
+++ src/test/java/Runner/RunnerClass.java
@@ -6,19 +6,12 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 
-/*author
-
-     
-     
-     samba
 
 
-
-
-*/
 @RunWith(Cucumber.class)
 @CucumberOptions(features ="C:\\Cucumber_Project\\CUCUMBER_POC\\src\\main\\java\\FeatureFile\\feat.feature", glue = {
-		"Stepdefination"},tags= {"@Test1","@Test2"})
+		"Stepdefination"},tags={"@Test2"})
+//tags= {"@Test1"}
 public class RunnerClass {
 
 }
